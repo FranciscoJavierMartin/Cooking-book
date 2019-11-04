e614a90735d4293e2c82b32463dc73c5c93501d7
Francisco <javiermartincuenca@gmail.com>
2019-11-04 12:41:14 +0100

    Custom buttons

--- src/screens/CategoryMealsScreen.tsx
+++ src/screens/CategoryMealsScreen.tsx
@@ -1,38 +1,46 @@
 import React from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, FlatList } from 'react-native';
 import {
   NavigationStackScreenComponent,
   NavigationStackScreenProps
 } from 'react-navigation-stack';
-import { CATEGORIES } from '../data/dummy-data';
 import Category from '../models/category';
+import Meal from '../models/meal';
+import { CATEGORIES, MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
-interface ICategoryMealsScreenProps
-  extends NavigationStackScreenProps {}
+interface ICategoryMealsScreenProps extends NavigationStackScreenProps {}
 
 const CategoryMealsScreen: NavigationStackScreenComponent<
   ICategoryMealsScreenProps
 > = (props: ICategoryMealsScreenProps) => {
+  const renderMealItem = ({ item }: { item: Meal }) => {
+    return (
+      <MealItem title={item.title} 
+      duration={item.duration}
+      complexity={item.complexity}
+      affordability={item.affordability}
+      image={item.imageUrl}
+      onSelectMeal={() => {
+        props.navigation.navigate({routeName: 'MealDetail', params: {
+          mealId: item.id
+        }})
+      }}/>
+    );
+  };
   const catId = props.navigation.getParam('categoryId');
   const selectedCategory = CATEGORIES.find((cat: Category) => cat.id === catId);
 
+  const displayedMeals = MEALS.filter(
+    (meal: Meal) => meal.categoryIds.indexOf(catId) >= 0
+  );
   return (
     <View style={styles.screen}>
-      <Text>The category meal screen</Text>
-      <Text>{selectedCategory.title}</Text>
-      <Button
-        title='Go to details'
-        onPress={() => {
-          props.navigation.navigate({
-            routeName: 'MealDetail'
-          });
-        }}
-      />
-      <Button
-        title='Go Back'
-        onPress={() => {
-          props.navigation.goBack();
-        }}
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item: Meal, index: number) => item.id}
+        renderItem={renderMealItem}
+        style={{width: '100%'}}
       />
     </View>
   );
--- src/screens/MealDetailScreen.tsx
+++ src/screens/MealDetailScreen.tsx
@@ -4,15 +4,21 @@ import {
   NavigationStackScreenComponent,
   NavigationStackScreenProps
 } from 'react-navigation-stack';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import HeaderButton from '../components/HeaderButton';
+import { MEALS } from '../data/dummy-data';
+import Meal from '../models/meal';
 
 interface IMealDetailScreenProps extends NavigationStackScreenProps {}
 
 const MealDetailScreen: NavigationStackScreenComponent = (
   props: IMealDetailScreenProps
 ) => {
+  const mealId = props.navigation.getParam('mealId');
+  const selectedMeal: Meal = MEALS.find((meal: Meal) => meal.id === mealId);
   return (
     <View style={styles.screen}>
-      <Text>The meal detail screen</Text>
+      <Text>{selectedMeal.title}</Text>
       <Button
         title='Go back to Categories'
         onPress={() => {
@@ -21,6 +27,21 @@ const MealDetailScreen: NavigationStackScreenComponent = (
       />
     </View>
   );
+};
+
+MealDetailScreen.navigationOptions = (
+  navigationData: NavigationStackScreenProps
+) => {
+  const mealId = navigationData.navigation.getParam('mealId');
+  const selectedMeal: Meal = MEALS.find((meal: Meal) => meal.id === mealId);
+  return {
+    headerTitle: selectedMeal.title,
+    headerRight: (
+      <HeaderButtons HeaderButtonComponent={HeaderButton}>
+        <Item title='Favorite' iconName='ios-star' onPress={() => {}}/>
+      </HeaderButtons>
+    )
+  };
 };
 
 const styles = StyleSheet.create({
